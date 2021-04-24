84aae96f60cbe6f24cab1d2db4028b40b64d4808
Ali Marasizadeh <marasiali@gmail.com>
2021-04-25 00:36:11 +0430

    PBE-6 Add ETag Support to APIs

--- pollgram_back/pollgram_back/settings.py
+++ pollgram_back/pollgram_back/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.http.ConditionalGetMiddleware',
 ]
 
 ROOT_URLCONF = 'pollgram_back.urls'
