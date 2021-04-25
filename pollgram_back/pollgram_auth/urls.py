cc54f0cf02f472bb467f05b0d7f8782eb0aca4ab
reza mehtari <remecom99@gmail.com>
2021-04-25 10:01:21 +0430

    PEB-3 support email verification

--- pollgram_back/pollgram_auth/urls.py
+++ pollgram_back/pollgram_auth/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 from dj_rest_auth.views import PasswordResetConfirmView
-from dj_rest_auth.registration.views import VerifyEmailView
 from dj_rest_auth.registration.views import VerifyEmailView, ConfirmEmailView
 
 
@@ -9,13 +8,14 @@ urlpatterns = [
     path(
         'registration/account-confirm-email/<str:key>/',
         ConfirmEmailView.as_view(),
-    ), 
-    path('registration/', include('dj_rest_auth.registration.urls')),
-    path('password/reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    ),
     path(
         'account-confirm-email/',
         VerifyEmailView.as_view(),
         name='account_email_verification_sent'
     ),
+    path('registration/', include('dj_rest_auth.registration.urls')),
+    path('password/reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
 
 ]
