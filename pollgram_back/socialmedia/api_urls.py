56fb00aaaabc1a7e2ba992cce70765b6eae3cc0b
amiralikhan079 <aligoli079@gmail.com>
2021-05-05 05:04:45 -0700

    Search impelemention

--- pollgram_back/socialmedia/api_urls.py
+++ pollgram_back/socialmedia/api_urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('user/<int:pk>/', user.UserAPIView.as_view(), name='user_api'),
     path('user/avatar/<int:pk>/', user.UserAvatarAPIView.as_view(), name='avatar_api'),
     path('user/cover/<int:pk>/', user.UserCoverAPIView.as_view(), name='cover_api'),
+
+    path('user/', user.UserListAPIView.as_view(), name='user_list_api'),
 ]
--- pollgram_back/socialmedia/views/user.py
+++ pollgram_back/socialmedia/views/user.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import get_user_model
-from rest_framework.generics import RetrieveUpdateDestroyAPIView, RetrieveUpdateAPIView
+from rest_framework import filters
+from rest_framework.authtoken.admin import User
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, RetrieveUpdateAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
+
+from .pagination import SearchResultsSetPagination
 from ..serializers.user import (
     UserAdminAccessSerializer,
     UserBaseAccessSerializer,
@@ -28,3 +32,18 @@ class UserCoverAPIView(RetrieveUpdateAPIView):
     queryset = get_user_model()
     serializer_class = UserCoverSerializer
     permission_classes = [IsAuthenticated, IsSelfOrReadOnly]
+
+class UserListAPIView(ListAPIView):
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated, IsSelfOrReadOnly]
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['username', 'first_name', 'last_name']
+
+    pagination_class = SearchResultsSetPagination
+
+    def get_serializer_class(self):
+        if self.request.user.is_superuser:
+            return UserAdminAccessSerializer
+        else:
+            return UserBaseAccessSerializer
