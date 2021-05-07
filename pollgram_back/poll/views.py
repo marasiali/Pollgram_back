8f55d4c5a61d2f26b4d57b7e7337ac9ad14095e0
reza mehtari <remecom99@gmail.com>
2021-05-07 12:38:00 +0430

    PBE-7 fixed vote create

--- pollgram_back/poll/views.py
+++ pollgram_back/poll/views.py
@@ -1,31 +1,19 @@
-from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from rest_framework import status
-from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveDestroyAPIView
+from rest_framework.generics import CreateAPIView, RetrieveDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Poll, Vote, Choice
-from .paginations import PollPagination
 from .permissions import IsCreatorOrReadOnly
-from .serializers import PollSerializer, VoteSerializer
+from .serializers import PollSerializer
 
 
 class PollRetrieveDestroyAPIView(RetrieveDestroyAPIView):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
     permission_classes = [IsAuthenticated, IsCreatorOrReadOnly]
-
-
-class PollListAPIView(ListAPIView):
-    pagination_class = PollPagination
-    serializer_class = PollSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        user = get_object_or_404(get_user_model(), id=self.kwargs['pk'])
-        return user.polls.all()
 
 
 class PollCreateAPIView(CreateAPIView):
@@ -35,10 +23,18 @@ class PollCreateAPIView(CreateAPIView):
 
 
 class VoteCreateAPIView(APIView):
-    def post(self, request, pk):
-        Vote.objects.create(user=request.user, selected=Choice.objects.get(id=pk))
-        return Response({
-            "status": "voted"
-        }, status=status.HTTP_201_CREATED)
-    serializer_class = VoteSerializer
     permission_classes = [IsAuthenticated]
+    def post(self, request, pk):
+        user = request.user
+        choice = get_object_or_404(Choice, pk=pk)
+        poll = choice.poll
+        if poll.choices.filter(votes__user=user).exists():
+            return Response({
+                "status": "already voted"
+            }, status=status.HTTP_409_CONFLICT)
+        created_vote = Vote.objects.create(user=user, selected=choice)
+        return Response({
+            "vote_id": created_vote.id,
+            "choice_id": created_vote.selected.id,
+            "user_id": created_vote.user.id
+        }, status=status.HTTP_201_CREATED)
