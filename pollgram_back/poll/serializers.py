6eda77a02c6b161eecf9c09467f550b0d1ed815d
reza mehtari <remecom99@gmail.com>
2021-05-18 15:50:54 +0430

    PBE-12 add serializers for file, image and link attachment

--- pollgram_back/poll/serializers.py
+++ pollgram_back/poll/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
 
-from poll.models import Poll, Choice, Vote
+from poll.models import Poll, Choice, Vote, File, Image
 from socialmedia.serializers.user import UserSummarySerializer
+
+
+class FileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = File
+        fields = ('id', 'file')
+        read_only_fields = ('id',)
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ('id', 'image')
+        read_only_fields = ('id',)
 
 
 class ChoiceSerializer(serializers.ModelSerializer):
@@ -14,13 +28,15 @@ class ChoiceSerializer(serializers.ModelSerializer):
         read_only_fields = ('id',)
 
 
-class PollSerializer(serializers.ModelSerializer):
+class PollCreateSerializer(serializers.ModelSerializer):
     creator = UserSummarySerializer(read_only=True)
     choices = ChoiceSerializer(many=True)
 
     class Meta:
         model = Poll
-        fields = ('id', 'created_at', 'question', 'description', 'creator', 'choices', 'is_commentable')
+        fields = (
+            'id', 'created_at', 'question', 'description', 'creator', 'choices', 'is_commentable', 'link', 'image',
+            'file', 'max_choice_can_vote', 'min_choice_can_vote', 'is_vote_retractable',)
         read_only_fields = ('id',)
 
     def create(self, validated_data):
@@ -32,9 +48,29 @@ class PollSerializer(serializers.ModelSerializer):
         return poll
 
 
+class PollRetrieveSerializer(serializers.ModelSerializer):
+    creator = UserSummarySerializer(read_only=True)
+    choices = ChoiceSerializer(many=True)
+    all_votes = serializers.SerializerMethodField('get_all_votes')
+    image = ImageSerializer()
+    file = FileSerializer()
+
+    class Meta:
+        model = Poll
+        fields = (
+            'id', 'created_at', 'question', 'description', 'creator', 'choices', 'is_commentable', 'link', 'image',
+            'file', 'max_choice_can_vote', 'min_choice_can_vote', 'is_vote_retractable', 'all_votes',)
+        read_only_fields = ('id',)
+
+    def get_all_votes(self, obj):
+        vote_counter = 0
+        for choice in obj.choices.all():
+            vote_counter += choice.votes.count()
+        return vote_counter
+
+
 class VoteSerializer(serializers.ModelSerializer):
-    user = serializers.PrimaryKeyRelatedField(read_only=True)
-    selected = serializers.PrimaryKeyRelatedField(read_only=True)
+    # user = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = Vote
