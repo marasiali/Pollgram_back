1640f68e2a75e051016879b6b1cd957bbb4c7e88
Ali Marasizadeh <marasiali@gmail.com>
2021-05-18 18:32:23 +0430

    PBE-27 Dockerize project and connect to postgres, gunicorn & nginx

--- pollgram_back/pollgram_back/urls.py
+++ pollgram_back/pollgram_back/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if not settings.IS_DOCKERIZED:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
