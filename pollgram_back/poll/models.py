00bc2c830ab9fc53a533031b34c95a3f324e869b
reza mehtari <remecom99@gmail.com>
2021-05-18 15:50:01 +0430

    PBE-12 add models for file, image and link attachment

--- pollgram_back/poll/models.py
+++ pollgram_back/poll/models.py
@@ -1,5 +1,18 @@
+import uuid
+
+from django.core.validators import MaxValueValidator
 from django.db import models
 from socialmedia.models import User
+
+
+class File(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, verbose_name='id', default=uuid.uuid4)
+    file = models.FileField(upload_to='files/%Y/%m/%d/' + str(id), blank=True, null=True)
+
+
+class Image(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, verbose_name='id', default=uuid.uuid4)
+    image = models.ImageField(upload_to='images/%Y/%m/%d/' + str(id), blank=True, null=True)
 
 
 class Poll(models.Model):
@@ -8,13 +21,26 @@ class Poll(models.Model):
     question = models.CharField(max_length=200)
     description = models.TextField()
     is_commentable = models.BooleanField(default=True)
+    image = models.ForeignKey(Image, on_delete=models.SET_NULL, related_name='polls', null=True)
+    file = models.ForeignKey(File, on_delete=models.SET_NULL, related_name='polls', null=True)
+    max_choice_can_vote = models.IntegerField(blank=True, default=1)
+    min_choice_can_vote = models.IntegerField(blank=True, default=1)
+    link = models.URLField(blank=True)
+    is_vote_retractable = models.BooleanField(default=True)
+
+    def get_image(self):
+        if Image.objects.filter(id=self.image.id).exits():
+            return Image.objects.get(id=self.image.id)
+
+    def get_file(self):
+        if File.objects.filter(id=self.file.id).exists():
+            return File.objects.get(id=self.file.id)
 
     class Meta:
         ordering = ('-created_at',)
 
     def __str__(self):
         return 'Poll = id: {}, creator: {}, question: {}'.format(self.id, self.creator, self.question)
-
 
 class Choice(models.Model):
     context = models.CharField(max_length=100)
