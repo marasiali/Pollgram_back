534cda7d980635d4c428403e34e098bf1c3d909e
reza mehtari <remecom99@gmail.com>
2021-05-19 20:36:34 +0430

    PBE-12 add file and image upload urls and change vote url

--- pollgram_back/poll/api_urls.py
+++ pollgram_back/poll/api_urls.py
@@ -5,6 +5,8 @@ app_name = 'poll'
 urlpatterns = [
     path('<int:pk>/', PollRetrieveDestroyAPIView.as_view()),
     path('', PollCreateAPIView.as_view()),
-    path('vote/<int:pk>/', VoteCreateAPIView.as_view())
+    path('vote/<int:poll_pk>/', VoteAPIView.as_view()),
+    path('image/', ImageCreateAPIView.as_view(), name='image'),
+    path('file/', FileCreateAPIView.as_view(), name='file')
 ]
 
