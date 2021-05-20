9efa7da0d58d104cc82af150f491b958c9d6dddf
reza mehtari <remecom99@gmail.com>
2021-05-20 19:41:14 +0430

    PBE-12 add url for getting voters

--- pollgram_back/poll/api_urls.py
+++ pollgram_back/poll/api_urls.py
@@ -3,9 +3,10 @@ from .views import *
 
 app_name = 'poll'
 urlpatterns = [
-    path('<int:pk>/', PollRetrieveDestroyAPIView.as_view()),
-    path('', PollCreateAPIView.as_view()),
-    path('vote/<int:poll_pk>/', VoteAPIView.as_view()),
+    path('<int:pk>/', PollRetrieveDestroyAPIView.as_view(), name='poll_retrieve'),
+    path('', PollCreateAPIView.as_view(), name='create_poll'),
+    path('vote/<int:poll_pk>/', VoteAPIView.as_view(), name='vote'),
+    path('<int:poll_pk>/choice/<int:order>/voters/', VotersListAPIView.as_view(), name='voters'),
     path('image/', ImageCreateAPIView.as_view(), name='image'),
     path('file/', FileCreateAPIView.as_view(), name='file')
 ]
