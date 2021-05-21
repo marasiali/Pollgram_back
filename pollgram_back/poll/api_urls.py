769cc559c92b33bc2003fc9791145f628b83c06f
reza mehtari <remecom99@gmail.com>
2021-05-21 16:31:33 +0430

    PBE-12 change some path name

--- pollgram_back/poll/api_urls.py
+++ pollgram_back/poll/api_urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 app_name = 'poll'
 urlpatterns = [
-    path('<int:pk>/', PollRetrieveDestroyAPIView.as_view(), name='poll_retrieve'),
-    path('', PollCreateAPIView.as_view(), name='create_poll'),
+    path('<int:pk>/', PollRetrieveDestroyAPIView.as_view(), name='poll_retrieve_destroy'),
+    path('', PollCreateAPIView.as_view(), name='poll_create'),
     path('vote/<int:poll_pk>/', VoteAPIView.as_view(), name='vote'),
     path('<int:poll_pk>/choice/<int:order>/voters/', VotersListAPIView.as_view(), name='voters'),
     path('image/', ImageCreateAPIView.as_view(), name='image'),
