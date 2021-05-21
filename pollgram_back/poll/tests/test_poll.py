16282f01c4842eaad7c2813a29e25849d030938b
reza mehtari <remecom99@gmail.com>
2021-05-21 12:28:00 +0430

    PBE-28 fixed tests

--- pollgram_back/poll/tests/test_poll.py
+++ pollgram_back/poll/tests/test_poll.py
@@ -17,44 +17,44 @@ class PollTest(APITestCase):
             "description": "about age",
             "choices": [
                 {
-                    "context": 48,
-                    "order": 1
+                    "context": "48",
+                    "order": "1"
                 },
                 {
-                    "context": 22,
-                    "order": 2
+                    "context": "22",
+                    "order": "2"
                 }
             ]
         }, HTTP_AUTHORIZATION=token, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual('how old are you?', response.json()['question'])
-        self.assertEqual('about age', response.json()['description'])
-        self.assertEqual(1, response.json()['creator']['id'])
-        self.assertEqual('ali', response.json()['creator']['username'])
-        self.assertEqual(2, len(response.json()['choices']))
-        self.assertEqual('48', response.json()['choices'][0]['context'])
-        self.assertEqual(1, response.json()['choices'][0]['order'])
-        self.assertEqual('22', response.json()['choices'][1]['context'])
-        self.assertEqual(2, response.json()['choices'][1]['order'])
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual('how old are you?', response.data['question'])
+        self.assertEqual('about age', response.data['description'])
+        self.assertEqual(1, response.data['creator']['id'])
+        self.assertEqual('ali', response.data['creator']['username'])
+        self.assertEqual(2, len(response.data['choices']))
+        self.assertEqual('48', response.data['choices'][0]['context'])
+        self.assertEqual(1, response.data['choices'][0]['order'])
+        self.assertEqual('22', response.data['choices'][1]['context'])
+        self.assertEqual(2, response.data['choices'][1]['order'])
 
     def test_get_poll(self):
         user = User.objects.get(id=2)
         token = f'Bearer {str(AccessToken.for_user(user))}'
         response = self.client.get('/api/poll/3/', HTTP_AUTHORIZATION=token)
         self.assertEqual(status.HTTP_200_OK, response.status_code)
-        self.assertEqual(3, response.json()['id'])
-        self.assertEqual("what's your favourite car", response.json()['question'])
-        self.assertEqual('car poll', response.json()['description'])
-        self.assertEqual(2, response.json()['creator']['id'])
-        self.assertEqual('sadra', response.json()['creator']['username'])
-        self.assertEqual(3, len(response.json()['choices']))
+        self.assertEqual(3, response.data['id'])
+        self.assertEqual("what's your favourite car", response.data['question'])
+        self.assertEqual('car poll', response.data['description'])
+        self.assertEqual(2, response.data['creator']['id'])
+        self.assertEqual('sadra', response.data['creator']['username'])
+        self.assertEqual(3, len(response.data['choices']))
 
     def test_delete_poll_not_creator_user(self):
         user = User.objects.get(id=1)
         token = f'Bearer {str(AccessToken.for_user(user))}'
         response = self.client.delete('/api/poll/3/', HTTP_AUTHORIZATION=token)
         self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
-        self.assertIsNotNone(Poll.objects.get(id=3))
+        self.assertTrue(Poll.objects.filter(id=3).exists())
 
     def test_delete_poll_creator_user(self):
         user = User.objects.get(id=2)
@@ -70,8 +70,8 @@ class PollTest(APITestCase):
         response = self.client.post('/api/poll/vote/2/', format='json', HTTP_AUTHORIZATION=token)
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
         self.assertEqual(vote_count_before + 1, Choice.objects.get(id=2).votes.count())
-        self.assertEqual(2, response.json()['choice_id'])
-        self.assertEqual(2, response.json()['user_id'])
+        self.assertEqual(2, response.data['choice_id'])
+        self.assertEqual(2, response.data['user_id'])
 
     def test_vote_already_voted(self):
         user = User.objects.get(id=2)
--- pollgram_back/socialmedia/tests/test_poll.py
+++ pollgram_back/socialmedia/tests/test_poll.py
@@ -8,24 +8,36 @@ from socialmedia.models import User
 class UserPollTest(APITestCase):
     fixtures = ['poll_users_fixture', 'poll_fixture']
 
-    def test_get_all_user_polls(self):
+    def test_get_user_polls(self):
         user = User.objects.get(id=1)
         token = f'Bearer {str(AccessToken.for_user(user))}'
         response = self.client.get('/api/user/2/polls/', HTTP_AUTHORIZATION=token)
         self.assertEqual(status.HTTP_200_OK, response.status_code)
-        self.assertEqual(2, response.json()['count'])
-        user_polls = response.json()['results']
+        self.assertEqual(2, response.data['count'])
+        user_polls = response.data['results']
         for poll in user_polls:
             self.assertEqual(2, poll['creator']['id'])
 
-    def test_get_timeline_owner_user(self):
+    def test_get_user_timeline(self):
         user = User.objects.get(id=3)
         token = f'Bearer {str(AccessToken.for_user(user))}'
         response = self.client.get('/api/user/timeline/', HTTP_AUTHORIZATION=token)
         self.assertEqual(status.HTTP_200_OK, response.status_code)
-        self.assertEqual(5, response.json()['count'])
-        polls = response.json()['results']
+        self.assertEqual(5, response.data['count'])
+        polls = response.data['results']
+        self.assertEqual(5, polls[0]['id'])
+        self.assertEqual(1, polls[0]['creator']['id'])
+        self.assertEqual(4, polls[1]['id'])
+        self.assertEqual(2, polls[1]['creator']['id'])
+        self.assertEqual(3, polls[2]['id'])
+        self.assertEqual(2, polls[2]['creator']['id'])
+        self.assertEqual(2, polls[3]['id'])
+        self.assertEqual(1, polls[3]['creator']['id'])
+        self.assertEqual(1, polls[4]['id'])
+        self.assertEqual(1, polls[4]['creator']['id'])
+
         create_times = []
         for poll in polls:
             create_times.append(poll['created_at'])
         self.assertTrue(create_times == sorted(create_times, reverse=True))
+
