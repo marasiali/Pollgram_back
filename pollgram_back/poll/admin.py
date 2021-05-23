5f5498ccb980203302a483f680f9f968efbb3f70
reza mehtari <remecom99@gmail.com>
2021-05-24 00:42:04 +0430

    PBE-12 add File and Image models to admin.py

--- pollgram_back/poll/admin.py
+++ pollgram_back/poll/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from poll.models import Poll, Vote, Choice
+from poll.models import Poll, Vote, Choice, Image, File
 
 admin.site.register(Poll)
 admin.site.register(Vote)
 admin.site.register(Choice)
+admin.site.register(File)
+admin.site.register(Image)
 
 # Register your models here.
