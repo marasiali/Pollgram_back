e9b8fae5469bdc2c2f9537856e5140ac439de293
reza mehtari <remecom99@gmail.com>
2021-05-24 01:01:38 +0430

    PBE-12 write test for get timeline and user polls

--- pollgram_back/socialmedia/tests/test_poll.py
+++ pollgram_back/socialmedia/tests/test_poll.py
@@ -6,7 +6,7 @@ from socialmedia.models import User
 
 
 class UserPollTest(APITestCase):
-    fixtures = ['poll_users_fixture', 'poll_fixture']
+    fixtures = ['poll_users_fixture', 'poll_fixture', 'poll_followRelationship_fixture']
 
     def test_get_user_polls(self):
         user = User.objects.get(id=1)
@@ -18,17 +18,25 @@ class UserPollTest(APITestCase):
         for poll in user_polls:
             self.assertEqual(2, poll['creator']['id'])
 
+        right_polls_order = [11, 8, 6, 5, 4, 3, 2]
+        for i in range(0, 7):
+            self.assertEqual(right_polls_order[i], user_polls[i]['id'])
+
+
     def test_get_user_timeline(self):
         user = User.objects.get(id=2)
         token = f'Bearer {str(AccessToken.for_user(user))}'
         response = self.client.get('/api/user/timeline/', HTTP_AUTHORIZATION=token)
         self.assertEqual(status.HTTP_200_OK, response.status_code)
-        self.assertEqual(7, response.data['count'])
+        self.assertEqual(10, response.data['count'])
         polls = response.data['results']
-
 
         create_times = []
         for poll in polls:
             create_times.append(poll['created_at'])
         self.assertTrue(create_times == sorted(create_times, reverse=True))
+
+        right_polls_order = [12, 11, 9, 8, 7, 6, 5, 4, 3, 2]
+        for i in range(0, 10):
+            self.assertEqual(right_polls_order[i], polls[i]['id'])
 
