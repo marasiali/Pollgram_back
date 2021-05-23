37183769daf23fb334c0e906f3e6a3673f7d5a9e
reza mehtari <remecom99@gmail.com>
2021-05-23 18:59:50 +0430

    PBE-12 fixed previous tests after change serializers

--- pollgram_back/socialmedia/tests/test_poll.py
+++ pollgram_back/socialmedia/tests/test_poll.py
@@ -13,8 +13,8 @@ class UserPollTest(APITestCase):
         token = f'Bearer {str(AccessToken.for_user(user))}'
         response = self.client.get('/api/user/2/polls/', HTTP_AUTHORIZATION=token)
         self.assertEqual(status.HTTP_200_OK, response.status_code)
-        self.assertEqual(3, len(response.data[0]['choices']))
-        user_polls = response.data
+        self.assertEqual(7, response.data['count'])
+        user_polls = response.data['results']
         for poll in user_polls:
             self.assertEqual(2, poll['creator']['id'])
 
@@ -23,9 +23,8 @@ class UserPollTest(APITestCase):
         token = f'Bearer {str(AccessToken.for_user(user))}'
         response = self.client.get('/api/user/timeline/', HTTP_AUTHORIZATION=token)
         self.assertEqual(status.HTTP_200_OK, response.status_code)
-        print(response.data)
-        self.assertEqual(7, len(response.data))
-        polls = response.data
+        self.assertEqual(7, response.data['count'])
+        polls = response.data['results']
 
 
         create_times = []
