027b72002003a7c75f2c77a21faba65b9671f4ee
reza mehtari <remecom99@gmail.com>
2021-05-27 13:54:52 +0430

    PBE-14 add Category to admin.py

--- pollgram_back/poll/admin.py
+++ pollgram_back/poll/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from poll.models import Poll, Vote, Choice, Image, File
+from poll.models import Poll, Choice, Image, File, Category
 
 admin.site.register(Poll)
 admin.site.register(Choice)
 admin.site.register(File)
 admin.site.register(Image)
+admin.site.register(Category)
 
 # Register your models here.
