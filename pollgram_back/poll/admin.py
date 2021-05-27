07b556dc1a4df9fb0423b805e103a9cd629c5299
amiralikhan079 <aligoli079@gmail.com>
2021-05-27 15:13:06 -0700

    PBE-8 add comment model to admin

--- pollgram_back/poll/admin.py
+++ pollgram_back/poll/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from poll.models import Poll, Choice, Image, File, Category
+from poll.models import Poll, Choice, Image, File, Category, Comment
 
 admin.site.register(Poll)
 admin.site.register(Choice)
 admin.site.register(File)
 admin.site.register(Image)
 admin.site.register(Category)
+admin.site.register(Comment)
 
 # Register your models here.
