2fc0ad29d93e2d94466747ce43e39f6b6bd43fcd
amiralikhan079 <aligoli079@gmail.com>
2021-05-27 15:13:42 -0700

    PBE-8 add comment urls

--- pollgram_back/poll/api_urls.py
+++ pollgram_back/poll/api_urls.py
@@ -10,6 +10,17 @@ urlpatterns = [
     path('image/', ImageCreateAPIView.as_view(), name='image'),
     path('file/', FileCreateAPIView.as_view(), name='file'),
     path('category/', CategoryListAPIView.as_view(), name='categories'),
-    path('category/<int:cat_pk>/', CategoryPollsListAPIView.as_view(), name='category_polls')
+    path('category/<int:cat_pk>/', CategoryPollsListAPIView.as_view(), name='category_polls'),
+
+    path('<int:poll_pk>/comments/', CommentListAPIView.as_view(), name='poll_comments_api'),
+    path('<int:poll_pk>/comment/filter/<int:order>', CommentFilterListAPIView.as_view(), name='poll_comments_filter_api'),
+
+    path('<int:poll_pk>/comment/', CommentAPIView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/', CommentRetrieveDestroyAPIView.as_view(), name='comment_retrieve_destroy'),
+
+    path('<int:poll_pk>/comment/<int:comment_pk>/', ReplyListAPIView.as_view(), name='comment_replies_api'),
+
+    path('<int:poll_pk>/comment/<int:comment_pk>/like/', LikeAPIView.as_view(), name='liked'),
+    path('<int:poll_pk>/comment/<int:comment_pk>/dislike/', DislikeAPIView.as_view(), name='disliked'),
 ]
 
