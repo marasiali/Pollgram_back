0c45cd61fb327145973049eb767f055229b93274
reza mehtari <remecom99@gmail.com>
2021-05-30 16:16:20 +0430

    PBE-11 add IsFollower permission to PollListAPIView

--- pollgram_back/socialmedia/views/user.py
+++ pollgram_back/socialmedia/views/user.py
@@ -16,7 +16,7 @@ from ..serializers.user import (
     UserAvatarSerializer,
     UserCoverSerializer, UserSummarySerializer,
 )
-from ..permissons import IsSelfOrReadOnly
+from ..permissons import IsSelfOrReadOnly, IsFollower
 
 
 class UserAPIView(RetrieveUpdateDestroyAPIView):
@@ -68,7 +68,7 @@ class UserTimelineListAPIView(ListAPIView):
 
 
 class PollListAPIView(ListAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsFollower]
     pagination_class = PollPagination
     serializer_class = PollRetrieveSerializer
 
