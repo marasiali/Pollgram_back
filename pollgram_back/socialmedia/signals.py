de5ed916fd04275b4b597b9d5f2a78801a9ed41c
reza mehtari <remecom99@gmail.com>
2021-05-30 23:05:41 +0430

    PBE-11 better filter for follow relationships in siganl

--- pollgram_back/socialmedia/signals.py
+++ pollgram_back/socialmedia/signals.py
@@ -7,6 +7,6 @@ from socialmedia.models import User, FollowRelationship
 @receiver(post_save, sender=User)
 def accept_all_follow_requests(sender, instance, created, **kwargs):
     if not created and instance.is_public:
-        follow_relationships = FollowRelationship.objects.filter(to_user=instance)
+        follow_relationships = FollowRelationship.objects.filter(to_user=instance, pending=True)
         if follow_relationships.exists():
             follow_relationships.update(pending=False)
