758d28785b6da0633f712434fdd2446f5804cb1c
reza mehtari <remecom99@gmail.com>
2021-05-30 19:37:06 +0430

    PBE-11 test signaling for accept all follow requests after set the page public

--- pollgram_back/socialmedia/tests/test_user.py
+++ pollgram_back/socialmedia/tests/test_user.py
@@ -3,6 +3,9 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from rest_framework_simplejwt.tokens import AccessToken
 
+from socialmedia.models import FollowRelationship
+
+
 class UserRetrieveAPITest(APITestCase):
     fixtures = ['user_fixture']
 
@@ -83,7 +86,6 @@ class UserRetrieveAPITest(APITestCase):
             "followings_count": 1
         })
 
-
     def test_retrieve_nonadmin_by_admin(self):
         admin_user = get_user_model().objects.get(id=1)
         token = f'Bearer {str(AccessToken.for_user(admin_user))}'
@@ -105,9 +107,10 @@ class UserRetrieveAPITest(APITestCase):
             "followings_count": 1
         })
 
+
 class UserUpdateAPITest(APITestCase):
     fixtures = ['user_fixture']
-    
+
     def test_update_admin_by_unauthorized(self):
         unauthorized_response = self.client.put('/api/user/1/')
         self.assertEqual(unauthorized_response.status_code, status.HTTP_401_UNAUTHORIZED)
@@ -121,8 +124,8 @@ class UserUpdateAPITest(APITestCase):
     def test_update_admin_by_admin(self):
         admin_user = get_user_model().objects.get(id=1)
         token = f'Bearer {str(AccessToken.for_user(admin_user))}'
-        admin_user_response = self.client.put('/api/user/1/',{
-            'id': 2, # can't change
+        admin_user_response = self.client.put('/api/user/1/', {
+            'id': 2,  # can't change
             "username": "admin1",
             "email": "admin1@pollgram.local",
             "first_name": "Admin1_first",
@@ -132,9 +135,9 @@ class UserUpdateAPITest(APITestCase):
             "bio": "Admin1 bio",
             "is_public": False,
             "is_verified": True,
-            "follow_status": "Pending", #can't changes
-            "followers_count": 100, # can't change
-            "followings_count": 100 # can't change
+            "follow_status": "Pending",  # can't changes
+            "followers_count": 100,  # can't change
+            "followings_count": 100  # can't change
         }, format='json', HTTP_AUTHORIZATION=token)
         self.assertEqual(admin_user_response.status_code, status.HTTP_200_OK)
         admin_user = get_user_model().objects.get(id=1)
@@ -164,20 +167,20 @@ class UserUpdateAPITest(APITestCase):
     def test_update_nonadmin_by_self(self):
         self_user = get_user_model().objects.get(id=2)
         token = f'Bearer {str(AccessToken.for_user(self_user))}'
-        self_user_response = self.client.put('/api/user/2/',{
-            "id": 1, # can't change
+        self_user_response = self.client.put('/api/user/2/', {
+            "id": 1,  # can't change
             "username": "sample1",
-            "email": "sample1@pollgram.local", # can't change
+            "email": "sample1@pollgram.local",  # can't change
             "first_name": "sample1_first",
             "last_name": "sample1_last",
             "avatar": "anything",
             "cover": "anything",
             "bio": "sample1 bio",
             "is_public": False,
-            "is_verified": True, # can't change
-            "follow_status": "Pending", #can't changes
-            "followers_count": 100, # can't change
-            "followings_count": 100 # can't change
+            "is_verified": True,  # can't change
+            "follow_status": "Pending",  # can't changes
+            "followers_count": 100,  # can't change
+            "followings_count": 100  # can't change
         }, format='json', HTTP_AUTHORIZATION=token)
         self.assertEqual(self_user_response.status_code, status.HTTP_200_OK)
         self_user = get_user_model().objects.get(id=2)
@@ -197,8 +200,8 @@ class UserUpdateAPITest(APITestCase):
     def test_update_nonadmin_by_admin(self):
         admin_user = get_user_model().objects.get(id=1)
         token = f'Bearer {str(AccessToken.for_user(admin_user))}'
-        admin_user_response = self.client.put('/api/user/2/',{
-            "id": 1, # can't change
+        admin_user_response = self.client.put('/api/user/2/', {
+            "id": 1,  # can't change
             "username": "sample1",
             "email": "sample1@pollgram.local",
             "first_name": "sample1_first",
@@ -208,9 +211,9 @@ class UserUpdateAPITest(APITestCase):
             "bio": "sample1 bio",
             "is_public": False,
             "is_verified": True,
-            "follow_status": "Pending", #can't changes
-            "followers_count": 100, # can't change
-            "followings_count": 100 # can't change
+            "follow_status": "Pending",  # can't changes
+            "followers_count": 100,  # can't change
+            "followings_count": 100  # can't change
         }, format='json', HTTP_AUTHORIZATION=token)
         self.assertEqual(admin_user_response.status_code, status.HTTP_200_OK)
         sample_user = get_user_model().objects.get(id=2)
@@ -227,9 +230,29 @@ class UserUpdateAPITest(APITestCase):
         self.assertEqual(sample_user.get_followers().count(), 0)
         self.assertEqual(sample_user.get_followings().count(), 1)
 
+    def test_signal_for_accepting_all_follow_requests_after_set_the_page_public(self):
+        # user 6 has two pending follow requests from user 2 and 3
+        user = get_user_model().objects.get(id=4)
+        token = f'Bearer {str(AccessToken.for_user(user))}'
+        response = self.client.put('/api/user/4/', {
+            "id": 4,  # can't change
+            "username": "hamid",
+            "email": "hamid@gmail.com",
+            "first_name": "hamid",
+            "last_name": "hor",
+            "is_public": True,
+        }, format='json', HTTP_AUTHORIZATION=token)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        follow_relationship = FollowRelationship.objects.filter(to_user=user)
+        self.assertEqual(2, follow_relationship[0].from_user.id)
+        self.assertFalse(follow_relationship[0].pending)
+        self.assertEqual(3, follow_relationship[1].from_user.id)
+        self.assertFalse(follow_relationship[1].pending)
+
+
 class UserDestroyAPITest(APITestCase):
     fixtures = ['user_fixture']
-    
+
     def test_destroy_admin_by_unauthorized(self):
         unauthorized_response = self.client.delete('/api/user/1/')
         self.assertEqual(unauthorized_response.status_code, status.HTTP_401_UNAUTHORIZED)
@@ -245,7 +268,7 @@ class UserDestroyAPITest(APITestCase):
         token = f'Bearer {str(AccessToken.for_user(admin_user))}'
         admin_user_response = self.client.delete('/api/user/1/', HTTP_AUTHORIZATION=token)
         self.assertEqual(admin_user_response.status_code, status.HTTP_204_NO_CONTENT)
-        
+
     def test_destroy_nonadmin_by_unauthorized(self):
         unauthorized_response = self.client.delete('/api/user/2/')
         self.assertEqual(unauthorized_response.status_code, status.HTTP_401_UNAUTHORIZED)
