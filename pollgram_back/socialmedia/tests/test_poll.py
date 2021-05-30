63d2c1f95cf7e70c1d5de1ca7fc04628296e12d8
reza mehtari <remecom99@gmail.com>
2021-05-30 20:10:25 +0430

    PBE-11 write test for get user polls

--- pollgram_back/socialmedia/tests/test_poll.py
+++ pollgram_back/socialmedia/tests/test_poll.py
@@ -6,9 +6,9 @@ from socialmedia.models import User
 
 
 class UserPollTest(APITestCase):
-    fixtures = ['poll_users_fixture', 'poll_fixture', 'poll_followRelationship_fixture']
+    fixtures = ['poll_users_fixture', 'poll_fixture']
 
-    def test_get_user_polls(self):
+    def test_get_public_page_polls(self):
         user = User.objects.get(id=1)
         token = f'Bearer {str(AccessToken.for_user(user))}'
         response = self.client.get('/api/user/2/polls/', HTTP_AUTHORIZATION=token)
@@ -20,6 +20,49 @@ class UserPollTest(APITestCase):
 
         right_polls_order = [11, 8, 6, 5, 4, 3, 2]
         for i in range(0, 7):
+            self.assertEqual(right_polls_order[i], user_polls[i]['id'])
+
+    def test_get_private_page_polls_when_not_followed(self):
+        # user 3 doesn't follow user 6
+        user = User.objects.get(id=3)
+        token = f'Bearer {str(AccessToken.for_user(user))}'
+        response = self.client.get('/api/user/6/polls/', HTTP_AUTHORIZATION=token)
+        self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
+
+    def test_get_private_page_polls_when_follow_request_not_accepted_yet(self):
+        # user 2 sent follow request for user 6 but user 6 doesn't accept yet
+        user = User.objects.get(id=2)
+        token = f'Bearer {str(AccessToken.for_user(user))}'
+        response = self.client.get('/api/user/6/polls/', HTTP_AUTHORIZATION=token)
+        self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
+
+    def test_get_private_page_polls_when_follow_request_accepted(self):
+        # user 1 sent follow request for user 6 and accepted
+        user = User.objects.get(id=1)
+        token = f'Bearer {str(AccessToken.for_user(user))}'
+        response = self.client.get('/api/user/6/polls/', HTTP_AUTHORIZATION=token)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        user_polls = response.data['results']
+        self.assertEqual(2, len(user_polls))
+        for poll in user_polls:
+            self.assertEqual(6, poll['creator']['id'])
+
+        right_polls_order = [13, 14]
+        for i in range(0, 2):
+            self.assertEqual(right_polls_order[i], user_polls[i]['id'])
+
+    def test_get_private_page_polls_by_page_owner(self):
+        user = User.objects.get(id=6)
+        token = f'Bearer {str(AccessToken.for_user(user))}'
+        response = self.client.get('/api/user/6/polls/', HTTP_AUTHORIZATION=token)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        user_polls = response.data['results']
+        self.assertEqual(2, len(user_polls))
+        for poll in user_polls:
+            self.assertEqual(6, poll['creator']['id'])
+
+        right_polls_order = [13, 14]
+        for i in range(0, 2):
             self.assertEqual(right_polls_order[i], user_polls[i]['id'])
 
     def test_get_user_timeline(self):
