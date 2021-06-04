3b390a2ebad24640d0028bbdb61960a6606e2106
Ali Marasizadeh <marasiali@gmail.com>
2021-06-04 11:32:12 +0430

    PBE-27 fix bug in deploy on port 80

--- pollgram_back/conf/nginx/entrypoint.sh
+++ pollgram_back/conf/nginx/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
 set -eu
-envsubst '$DEPLOY_PORT' < /tmp/default-raw.conf > /etc/nginx/conf.d/default.conf
+
+if [ "$DEPLOY_PORT" -ne "80" ]; then
+   export DEPLOY_PORT_COLON=":${DEPLOY_PORT}"
+fi
+
+envsubst '$DEPLOY_PORT_COLON' < /tmp/default-raw.conf > /etc/nginx/conf.d/default.conf
 exec "$@"
