321d9bcf43ea1733996804046e8a45f9c215d360
Ali Marasizadeh <marasiali@gmail.com>
2021-06-04 12:01:21 +0430

    PBE-27 Fix static url conflict between backend & frontend

--- pollgram_back/conf/nginx/entrypoint.sh
+++ pollgram_back/conf/nginx/entrypoint.sh
@@ -5,5 +5,5 @@ if [ "$DEPLOY_PORT" -ne "80" ]; then
    export DEPLOY_PORT_COLON=":${DEPLOY_PORT}"
 fi
 
-envsubst '$DEPLOY_PORT_COLON' < /tmp/default-raw.conf > /etc/nginx/conf.d/default.conf
+envsubst '$DEPLOY_PORT_COLON $DJANGO_STATIC_URL' < /tmp/default-raw.conf > /etc/nginx/conf.d/default.conf
 exec "$@"
--- pollgram_back/pollgram_back/settings.py
+++ pollgram_back/pollgram_back/settings.py
@@ -153,7 +153,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/static/")
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
